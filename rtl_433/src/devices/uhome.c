6d364b7d2f49e4844559c27a8f609692fe2a34f7
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2018-04-13 02:44:25 +0200

    Refactoring:
    * check crc or magic number before processing payload
    * extract crc calculation function
    * verify temperature and humidity sensor readings, set to "-1.0" on failure
    * return number of successfully read messages

--- rtl_433/src/devices/uhome.c
+++ rtl_433/src/devices/uhome.c
@@ -9,6 +9,7 @@
  * (at your option) any later version.
  */
 
+#include <stdbool.h>
 #include "rtl_433.h"
 #include "pulse_demod.h"
 #include "util.h"
@@ -24,8 +25,31 @@
 #define UHOME_BITLEN      64
 #define UHOME_STARTBYTE   0xAA
 #define UHOME_STARTBYTE1  0xFF
+#define UHOME_CRC_SUPPORT 0xA1
 #define UHOME_CRC_POLY    0xB1
 #define UHOME_CRC_INIT    0x00
+
+uint8_t calc_crc(uint8_t *bb) {
+	uint8_t crc = bb[1];
+
+	// resetting crc in buf for calc
+	bb[1] = 0xFF;
+
+	uint8_t i, j, crcc = 0;
+	for (i = 0; i < 10; i++) {
+		crcc ^= bb[i];
+		for (j = 0; j < 8; j++) {
+			if (crcc & 0x01) {
+				crcc ^= UHOME_CRC_POLY;
+			}
+
+			crcc >>= 1;
+		}
+	}
+	// restore the byte we reset earlier
+	bb[1] = crc;
+	return crcc;
+}
 
 static int uhome_callback(bitbuffer_t *bitbuffer) {
     char time_str[LOCAL_TIME_BUFLEN];
@@ -33,9 +57,9 @@ static int uhome_callback(bitbuffer_t *bitbuffer) {
     uint16_t brow, row_nbytes;
     uint16_t sensor_id = 0;
     uint8_t msg_type, r_crc, c_crc;
-    double temp;
+    double temp = -1.0;
     double voltage;
-    double humidity;
+    double humidity = -1.0;
     data_t *data;
     int valid = 0;
 
@@ -96,66 +120,62 @@ static int uhome_callback(bitbuffer_t *bitbuffer) {
     if (bb[0] != UHOME_STARTBYTE)
         continue;
 
-    //if (bb[1] != UHOME_STARTBYTE1)
-    //    continue;
-
     /*
-     * Now that message "envelope" has been validated,
-     * start parsing data.
+     * Check CRC or magic number before inspecting rest of the payload
      */
-
-    // check the sensor type
-    if (bb[2] == 0xAB) {
-        // sensor is a temp/hum sensor
-        sensor_id = (bb[2]<<8)|bb[3];
-        int16_t it = (bb[4]<<8)|bb[5];
-        temp = (175.72*it)/65536-46.85;
-        int16_t iv = (bb[7]<<8)|bb[6];
-        voltage = iv/1000.0;
-
-        int16_t ih = (bb[8]<<8)|bb[9];
-        humidity = (125.0*ih)/65536-6.0;
-
 	// sensors bigger A0 have CRC support
-	if ((sensor_id>>8) > 0xA0) {
-		uint8_t crc = bb[1];
-		fprintf(stderr, "CRC: %02x\n", crc);
-
-		// resetting crc in buf for calc
-		bb[1] = 0xFF;
-
+	if (bb[2] >= UHOME_CRC_SUPPORT) {
 		if (row_nbytes < 10) {
 			fprintf(stderr, "uhome: buffer too short, was: %u, should be 10\n", row_nbytes);
 			continue;
 		}
 
-
-		uint8_t i, j, crcc = 0;
-		for (i = 0; i < 10; i++) {
-			crcc ^= bb[i];
-			for (j = 0; j < 8; j++) {
-				if (crcc & 0x01) {
-					crcc ^= UHOME_CRC_POLY;
-				}
-
-				crcc >>= 1;
-			}
-		}
+        uint8_t crc = bb[1];
+		uint8_t crcc = calc_crc(bb);
 
 		if (crcc != crc) {
-			// example debugging output
-			fprintf(stderr, "%s new_tamplate bad CRC: calculated %02x, received %02x\n",
+			fprintf(stderr, "%s new_template bad CRC: calculated %02x, received %02x\n",
 			    time_str, crcc, crc);
 
-			// reject row
-//			continue;
+			continue;
 		} else {
-			fprintf(stderr, "CRC matches\n");
+			if (debug_output > 1) {
+				fprintf(stderr, "CRC matches (0x%02x)\n", crc);
+			}
 		}
 	} else if (bb[1] != UHOME_STARTBYTE1) {
 		fprintf(stderr, "%02x does not match startbyte 0xFF\n", bb[1]);
 		continue;
 	}
+
+    /*
+     * Now that message "envelope" has been validated,
+     * start parsing data.
+     */
+    // check the sensor type
+    if (bb[2] == 0xAB) {
+        // sensor is a temp/hum sensor
+        sensor_id = (bb[2]<<8)|bb[3];
+
+        // temperature
+        // 2 status bits: 00
+        if((bb[5] & 0x03) == 0) {
+            int16_t it = (bb[4]<<8)|bb[5];
+            temp = (175.72*it)/65536-46.85;
+        }
+
+        // voltage
+        int16_t iv = (bb[7]<<8)|bb[6];
+        voltage = iv/1000.0;
+
+        // humidity
+        // 2 status bits: 10
+        if((bb[9] & 0x03) == 0x02) {
+            int16_t ih = (bb[8]<<8)|bb[9];
+            // zero out status bits
+            ih &= 0xFFFC;
+            humidity = (125.0*ih)/65536-6.0;
+        }
 
         data = data_make("time", "", DATA_STRING, time_str,
         "model", "", DATA_STRING, "µHome",
@@ -168,10 +188,16 @@ static int uhome_callback(bitbuffer_t *bitbuffer) {
     } else if (bb[2] == 0xAA) {
         // sensor is a temperature sensor
         sensor_id = (bb[2]<<8)|bb[3];
-        int16_t it = (bb[4]<<8)|bb[5];
-        temp = (175.72*it)/65536-46.85;
-        int16_t iv = (bb[7]<<8)|bb[6];
 
+        // temperature
+        // 2 status bits: 00
+        if((bb[5] & 0x03) == 0) {
+            int16_t it = (bb[4]<<8)|bb[5];
+            temp = (175.72*it)/65536-46.85;
+        }
+
+        // voltage
+        int16_t iv = (bb[7]<<8)|bb[6];
         voltage = iv/1000.0;
 
         data = data_make("time", "", DATA_STRING, time_str,
@@ -190,11 +216,8 @@ static int uhome_callback(bitbuffer_t *bitbuffer) {
 
     }
 
-    // Return 1 if message successfully decoded
-    if (valid)
-    return 1;
-
-    return 0;
+    // Return number of successfully decoded messages
+    return valid;
 }
 
 /*
